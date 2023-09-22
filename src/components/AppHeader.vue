09436270d2c0093c473e503d523e172361237575
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2023-09-23 02:21:11 +0530

    adds footer

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="navbar bg-base-100">
         <div class="flex-1">
-            <a class="btn btn-ghost normal-case text-xl">BookBreeze</a>
+            <a class="btn btn-ghost normal-case text-xl">
+                <img src="@/assets/logo.png" alt="logo" width="30" height="30">
+                BookBreeze
+            </a>
         </div>
         <div v-if="showLoginButton" class="flex-none">
             <ul class="menu menu-horizontal px-1">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,18 @@
   <nav-bar></nav-bar>
 
   main content here
-  app footer
+  <app-footer></app-footer>
 </template>
 
 <script>
 import NavBar from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    NavBar
+    NavBar,
+    AppFooter
   }
 }
 </script>
